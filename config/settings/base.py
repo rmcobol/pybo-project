37ac77435ec72897c26a5d5ede44e114aa4009a6
rmcobol <lstrmcobol123@naver.com>
2024-04-03 13:49:26 +0900

    settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure--dbon_ht+4z+vw@)q=+oz!kwoy-a^$*tq)193i1&qx+_gb96gb
 DEBUG = True
 
 ALLOWED_HOSTS = ['13.124.49.162']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
