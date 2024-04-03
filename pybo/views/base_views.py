8cd46b1fbbae1c0d9ada01697df5411b56f4eaa9
rmcobol <lstrmcobol123@naver.com>
2024-04-03 19:47:03 +0900

    zero error

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from ..models import Question
 
 def index(request):
+    3/0 # 강제로 오류 발생
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
     page = request.GET.get('page','1') # 페이지
     kw = request.GET.get('kw','') # 검색어
